648a30f7fb4472fd5b08cf816e40bce39fe32548
Tazkia Jessy <tazkia.jessy@and.digital>
2021-08-24 12:18:31 +0200

    API call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 // import logo from './logo.svg';
-import './App.css';
-import TitleForm from './Components/TitleForm/TitleForm';
+import "./App.css";
+import TitleForm from "./Components/TitleForm/TitleForm";
+// const axios = require("axios").default;
 
 function App() {
-
   const handleSubmit = (event) => {
     event.preventDefault();
-}
+  };
+
+  // axios
+  //   .get("https://us-central1-and-forms.cloudfunctions.net/sendQuestionnaire")
+  //   .then(function (response) {
+  //     // handle success
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     // handle error
+  //     console.log(error);
+  //   })
+  //   .then(function () {
+  //     // always executed
+  //   });
+
+  // async function getData() {
+  //   const response = await fetch(
+  //     "https://us-central1-and-forms.cloudfunctions.net/sendQuestionnaire"
+  //   );
+  //   const data = await response.json();
+  //   return data;
+  //
+  // getData();
 
   return (
     <div className="App">
-      <TitleForm handleSubmit={handleSubmit}/>
+      <TitleForm handleSubmit={handleSubmit} />
     </div>
   );
 }
--- src/Components/TitleForm/TitleForm.js
+++ src/Components/TitleForm/TitleForm.js
@@ -1,49 +1,73 @@
-import React from 'react';
-import {Form, Button} from 'react-bootstrap';
-import './TitleForm.css'
-import { useState } from 'react';
-import moment from 'moment';
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import "./TitleForm.css";
+import { useState } from "react";
+import moment from "moment";
 
-const TitleForm = ({handleSubmit}) => {
-    const [titleText, setTitleText] = useState('');
-    const [email, setEmail] = useState('');
+const TitleForm = ({ handleSubmit }) => {
+  const [titleText, setTitleText] = useState("");
+  const [email, setEmail] = useState("");
 
-    const handleTitleChange = (e) => {
-        setTitleText(e.target.value);
-    }
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    }
+  const handleTitleChange = (e) => {
+    setTitleText(e.target.value);
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const [startDate, setStartDate] = useState(moment(new Date()).format("YYYY-MM-DD"));
+  const [startDate, setStartDate] = useState(
+    moment(new Date()).format("YYYY-MM-DD")
+  );
 
-    const handleNewDate = (evt) => {
-        setStartDate(evt.target.value)
-    }
+  const handleNewDate = (evt) => {
+    setStartDate(evt.target.value);
+  };
 
-    return (
-        <div className="d-flex justify-content-center">
-            <Form className="event-name">
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Name of Event</Form.Label>
-                    <Form.Control placeholder="Club Team Building" onChange={handleTitleChange} value={titleText} type="text" required />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Email To</Form.Label>
-                    <Form.Control placeholder="name@example.com" onChange={handleEmailChange} value={email} type="email" required />
-                </Form.Group>
-                <div className="date-picker" style={{marginTop: "5px"}}>
-                <label  htmlFor="start">Start date:</label>
-                <div >
-                <input  onChange={handleNewDate} type="date" id="start" name="trip-start"
-                    value={startDate}>
-                </input>
-                </div>
-                </div>
-                <Button onClick={handleSubmit} type="submit" className="submit-button w-100" >Send</Button>
-            </Form>
-      </div>
-    )
-}
+  return (
+    <div className="d-flex justify-content-center">
+      <Form className="event-name">
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Name of Event</Form.Label>
+          <Form.Control
+            placeholder="Club Team Building"
+            onChange={handleTitleChange}
+            value={titleText}
+            type="text"
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Email To</Form.Label>
+          <Form.Control
+            placeholder="name@example.com"
+            onChange={handleEmailChange}
+            value={email}
+            type="email"
+            required
+          />
+        </Form.Group>
+        <div className="date-picker" style={{ marginTop: "5px" }}>
+          <label htmlFor="start">Start date:</label>
+          <div>
+            <input
+              onChange={handleNewDate}
+              type="date"
+              id="start"
+              name="trip-start"
+              value={startDate}
+            ></input>
+          </div>
+        </div>
+        <Button
+          onClick={handleSubmit}
+          type="submit"
+          className="submit-button w-100"
+        >
+          Send
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
-export default TitleForm
+export default TitleForm;
