326e8621dafd92a98659a3b354da05977056beea
Corey McCue <corey@Coreys-MacBook-Pro.local>
2021-08-24 13:43:00 +0200

    api get request working

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 // import logo from './logo.svg';
 import "./App.css";
 import TitleForm from "./Components/TitleForm/TitleForm";
-// const axios = require("axios").default;
+const axios = require("axios").default;
 
 function App() {
   const handleSubmit = (event) => {
     event.preventDefault();
   };
 
-  // axios
-  //   .get("https://us-central1-and-forms.cloudfunctions.net/sendQuestionnaire")
-  //   .then(function (response) {
-  //     // handle success
-  //     console.log(response);
-  //   })
-  //   .catch(function (error) {
-  //     // handle error
-  //     console.log(error);
-  //   })
-  //   .then(function () {
-  //     // always executed
-  //   });
-
-  // async function getData() {
-  //   const response = await fetch(
-  //     "https://us-central1-and-forms.cloudfunctions.net/sendQuestionnaire"
-  //   );
-  //   const data = await response.json();
-  //   return data;
-  //
-  // getData();
+  axios
+    .get("https://us-central1-and-forms.cloudfunctions.net/sendQuestionnaire")
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
 
   return (
     <div className="App">
