e02d42c8e9ccfb32a29b322cdbedd899a68461a9
Corey McCue <corey@Coreys-MacBook-Pro.local>
2021-08-24 09:24:15 +0200

    basic date picker functionality

--- src/Components/DatePicker/DatePicker.js
+++ src/Components/DatePicker/DatePicker.js
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 // import DatePicker from "react-datepicker";
 import './DatePicker.css';
+import moment from 'moment';
 
 import "react-datepicker/dist/react-datepicker.css";
 
 const PickDate = () => {
-    // const [startDate, setStartDate] = useState(new Date());
+    const [startDate, setStartDate] = useState(moment(new Date()).format("YYYY-MM-DD"));
+
+    const handleNewDate = (evt) => {
+        setStartDate(evt.target.value)
+    }
     return (
         <div className="date-picker" style={{ marginTop: "1%"}}>
-            <label style={{paddingLeft: "20%"}} for="start">Start date:</label>
-            {/* <DatePicker
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-            /> */}
+            <label style={{paddingLeft: "20%"}} htmlFor="start">Start date:</label>
         <div style={{paddingLeft: "42%"}}>
-            <input type="date" id="start" name="trip-start"
-                value="2018-07-22"
-                min="2018-01-01" max="2018-12-31">
+            <input onChange={handleNewDate} type="date" id="start" name="trip-start"
+                value={startDate}
+            >
             </input>
         </div>
         </div>
