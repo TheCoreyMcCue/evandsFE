a10ebf71a9277375872d0ed12e1d3d52ce3bcfbe
Corey McCue <corey@Coreys-MacBook-Pro.local>
2021-08-24 16:38:04 +0200

    layout of page and trying to get POST request to work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // import logo from './logo.svg';
 import "./App.css";
 import TitleForm from "./Components/TitleForm/TitleForm";
+import ProgressBar from "./Components/ProgressBar/ProgressBar";
+import {Card, Button} from 'react-bootstrap';
+
 const axios = require("axios").default;
 
 function App() {
@@ -8,23 +11,54 @@ function App() {
     event.preventDefault();
   };
 
-  axios
-    .get("https://us-central1-and-forms.cloudfunctions.net/sendQuestionnaire")
-    .then(function (response) {
-      // handle success
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
+  // axios
+  //   .get("https://us-central1-and-forms.cloudfunctions.net/sendQuestionnaire")
+  //   .then(function (response) {
+  //     // handle success
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     // handle error
+  //     console.log(error);
+  //   })
+  //   .then(function () {
+  //     // always executed
+  //   });
+
+  let config = {
+    headers: {
+      "Content-Type": "application/json",
+      }
+    }
+
+    let data = {
+      "title": "test"
+     }
+
+  const send = () => {
+    axios.post('https://us-central1-and-forms.cloudfunctions.net/sendQuestionnaire', data, config)
+   .then((res) => {
+      console.log(res)
+     })
+    .catch((err) => {
+      console.log(err)
+   });
+}
+
+
+
+
+
 
   return (
     <div className="App">
-      <TitleForm handleSubmit={handleSubmit} />
+      <Card style={{width: "60%", position: "absolute", top: "20%", left: "20%"}}>
+        <Card.Body>
+          <ProgressBar/>
+          <TitleForm handleSubmit={handleSubmit} />
+          <Button onClick={send}>post</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
---
+++ src/Components/DropDown/DropDown.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Dropdown } from 'react-bootstrap';
+
+const DropDown = () => {
+    return (
+        <Dropdown className="mb-1 mt-2">
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Add Question
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Multi-line</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Checkbox</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Radio Button</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    )
+}
+
+export default DropDown
