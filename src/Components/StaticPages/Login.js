47f4cf83052fcb9e5ceac103e9f464dec6511acd
Corey McCue <corey@Coreys-MacBook-Pro.local>
2021-08-26 11:47:55 +0200

    quick style on login page

--- src/Components/StaticPages/Login.js
+++ src/Components/StaticPages/Login.js
@@ -5,30 +5,30 @@ import './StaticPages.css';
 
 const Login = () => {
     return (
-        <Card>
-            <div className="container d-flex login">
-                <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+        <div>
+            <h1 className="login-head">evANDs Event Creator</h1>
+            <Card className="login-card">
+                <div className="container login">
+                    <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-                </Form>
-            </div>
-        </Card>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" href="/">
+                        Submit
+                    </Button>
+                    </Form>
+                </div>
+            </Card>
+        </div>
     )
 }
 
