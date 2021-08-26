d3ecaa6dc9fe3045dea88697dbd3480e0bb64f4e
Richard Corke <rich@Richards-MacBook-Pro.local>
2021-08-26 11:09:00 +0200

    add confirmedsent static page

---
+++ src/Components/StaticPages/ConfirmedSent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ConfirmedSent = () => {
+    return (
+        <div>
+            <p>
+                Thank you for sending in your Preference Survey!
+            </p>
+        </div>
+    )
+}
+
+export default ConfirmedSent
