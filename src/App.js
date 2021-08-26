0cf935356d835f01a0e06b1db2c2264d2aafe7a9
Richard Corke <rich@Richards-MacBook-Pro.local>
2021-08-26 18:01:19 +0200

    add dynamic response page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import MainMenu from "./Components/StaticPages/MainMenu";
 import Logout from "./Components/StaticPages/Logout";
 import { Card } from "react-bootstrap";
 import {BrowserRouter as Router,Route,} from "react-router-dom";
+import Response from './Components/Response/Reponse';
 
 // const axios = require("axios").default;
 
@@ -33,6 +34,9 @@ function App() {
     <Router>
       <Route exact path="/login">
           <Login />
+      </Route>
+      <Route exact path="/response">
+          <Response />
       </Route>
       <Route exact path="/confirmed">
           <ConfirmedResponseSent />
