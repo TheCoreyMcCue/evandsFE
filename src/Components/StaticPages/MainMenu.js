74d561be6770b460a90be7aa59b1dfa28942c172
Richard Corke <rich@Richards-MacBook-Pro.local>
2021-08-26 13:49:59 +0200

    fix some styling issues

--- src/Components/StaticPages/MainMenu.js
+++ src/Components/StaticPages/MainMenu.js
@@ -1,18 +1,24 @@
 import React from 'react'
-import {Button} from 'react-bootstrap';
+import {Button, Card} from 'react-bootstrap';
+import './StaticPages.css';
+
 
 const MainMenu = () => {
     return (
         <div>
-            <h1>
-                Welcome to EvANDs!
-            </h1>
-            <h2>
-                <Button type="button" href="/create_event">Create an Event</Button>
-            </h2>
-            <p>
-                <Button type="button" href="/logout">Logout</Button>
-            </p>
+            <Card className="login-card">
+                <h1 className="main-menu-welcome">
+                    Welcome to EvANDs!
+                </h1>
+                <div className="main-menu">
+                    <h2 className="main-menu-create-button">
+                        <Button size="lg" href="/create_event">Create an Event</Button>
+                    </h2>
+                    <h3 className="main-menu-logout-button">
+                        <Button variant="secondary" href="/logout">Logout</Button>
+                    </h3>
+                </div>
+            </Card>
         </div>
     )
 }
