f1c868e727b3eed400b8aa7274ba13ca3be3144e
Richard Corke <rich@Richards-MacBook-Pro.local>
2021-08-26 11:14:24 +0200

    add confirmed response sent page

---
+++ src/Components/StaticPages/ConfirmedResponseSent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ConfirmedResponseSent = () => {
+    return (
+        <div>
+            <h1>
+                Thank you
+            </h1>
+            <p>
+                Your response has been submitted. See you at the event!
+            </p>
+        </div>
+    )
+}
+
+export default ConfirmedResponseSent
