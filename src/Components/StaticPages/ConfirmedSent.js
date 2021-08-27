dc3657363df0b2b7a889a624d9fd5aa14e259c8b
Corey McCue <corey@Coreys-MacBook-Pro.local>
2021-08-27 07:59:35 +0200

    done

--- src/Components/StaticPages/ConfirmedSent.js
+++ src/Components/StaticPages/ConfirmedSent.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ConfirmedSent = () => {
     return (
-<div class="alert alert-success text-center mt-1" role="alert">
-  Thank you for submitting your preferences!
-</div>
+        <div class="alert alert-success text-center mt-1" role="alert">
+        Thank you for submitting your preferences!
+        </div>
     )
 }
 
